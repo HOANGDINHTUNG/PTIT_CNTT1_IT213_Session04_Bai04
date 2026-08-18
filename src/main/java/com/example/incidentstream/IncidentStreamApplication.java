d60d29bd56f966fcc7e143121d2f897f09183843
unknown <mtung3365@gmail.com>
2026-08-18 08:07:24 +0700

    Fix Spring AI RestClient Bean in WebFlux

--- src/main/java/com/example/incidentstream/IncidentStreamApplication.java
+++ src/main/java/com/example/incidentstream/IncidentStreamApplication.java
@@ -3,6 +3,9 @@ package com.example.incidentstream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+
 @SpringBootApplication
 public class IncidentStreamApplication {
 
@@ -10,4 +13,12 @@ public class IncidentStreamApplication {
 		SpringApplication.run(IncidentStreamApplication.class, args);
 	}
 
+	@Bean
+	public RestClient.Builder restClientBuilder() {
+		return RestClient.builder();
+	}
+
+
 }
+
+
